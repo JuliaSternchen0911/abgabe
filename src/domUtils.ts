8ec9ec9ca940e40a1c1564457dba643196b6b9a4
JuliaSternchen0911 <petersjulia0911@gmail.com>
2023-04-18 11:02:57 +0200

    Html und css Grundbau

---
+++ src/domUtils.ts
@@ -0,0 +1,7 @@
+//Array für die Elemente erstellen
+let pflanze :String[] = [];
+let pflanzenname = document.querySelector("#pflanzenname") as HTMLInputElement;
+let gießintervall = document.querySelector("#gießintervall") as HTMLInputElement;
+let standort = document.querySelector("#standort") as HTMLInputElement;
+let pflanzenliste = "";
+export{pflanze,pflanzenname,standort,gießintervall,pflanzenliste};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+import { pflanze, pflanzenname, standort, gießintervall,pflanzenliste } from 'domUtils';
+
+//Funktion zum Hinzufügen einer neuen Pflanze
+function pflanzeHinzufuegen() {
+
+    //Pflanzenobjekt erstellen
+    pflanze = {
+        name: pflanzenname,
+        gießintervall: gießintervall,
+        standort: standort
+    };
+
+    //Pflanze zum Array hinzufügen
+    pflanze.push(pflanze);
+
+    //Pflanzenliste aktualisieren
+    pflanzenlisteAktualisieren();
+
+    //Formular zurücksetzen
+    pflanzenname = "";
+    gießintervall= "";
+    standort = "Wohnzimmer";
+}
+
+//Funktion zum Aktualisieren der Pflanzenliste
+function pflanzenlisteAktualisieren() {
+    
+         pflanzenliste = document.getElementById("pflanzenliste");
+
+        //Pflanzenliste leeren
+        pflanzenliste.innerHTML = "";
+        let pflanzenliste = document.getElementById("pflanzenliste");
+        //Für jede Pflanze in der Liste eine Listeneintrag erstellen
+        for (let i = 0; i < pflanzen.length; i++) {
+            let pflanze = pflanzen[i];
+
+            //Listeneintrag erstellen
+            let eintrag = document.createElement("li");
+            eintrag.innerHTML = "<b>" + pflanze.name + "</b> (" + pflanze.standort + ") - gießen alle " + pflanze.gießintervall + " Tage";
+
+            //Listeneintrag zur Pflanzenliste hinzufügen
+            pflanzenliste.appendChild(eintrag);
+
+        }
+    }
+}
