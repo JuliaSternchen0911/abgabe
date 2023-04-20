7c24d056f010d7cc1d7b0162519b00eae2a940a9
JuliaSternchen0911 <petersjulia0911@gmail.com>
2023-04-20 11:35:21 +0200

    pflegeanleitung

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,70 @@
-import { pflanze, pflanzenname, standort, gießintervall,pflanzenliste } from 'domUtils';
-
-//Funktion zum Hinzufügen einer neuen Pflanze
-function pflanzeHinzufuegen() {
-
-    //Pflanzenobjekt erstellen
-    pflanze = {
-        name: pflanzenname,
-        gießintervall: gießintervall,
-        standort: standort
-    };
-
-    //Pflanze zum Array hinzufügen
-    pflanze.push(pflanze);
-
-    //Pflanzenliste aktualisieren
-    pflanzenlisteAktualisieren();
-
-    //Formular zurücksetzen
-    pflanzenname = "";
-    gießintervall= "";
-    standort = "Wohnzimmer";
-}
-
-//Funktion zum Aktualisieren der Pflanzenliste
-function pflanzenlisteAktualisieren() {
-    
-         pflanzenliste = document.getElementById("pflanzenliste");
-
-        //Pflanzenliste leeren
-        pflanzenliste.innerHTML = "";
-        let pflanzenliste = document.getElementById("pflanzenliste");
-        //Für jede Pflanze in der Liste eine Listeneintrag erstellen
-        for (let i = 0; i < pflanzen.length; i++) {
-            let pflanze = pflanzen[i];
-
-            //Listeneintrag erstellen
-            let eintrag = document.createElement("li");
-            eintrag.innerHTML = "<b>" + pflanze.name + "</b> (" + pflanze.standort + ") - gießen alle " + pflanze.gießintervall + " Tage";
-
-            //Listeneintrag zur Pflanzenliste hinzufügen
-            pflanzenliste.appendChild(eintrag);
-
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    const addButton = document.getElementById("add-plant") as HTMLButtonElement;
+    addButton.onclick = addPlant;
+  
+    setPlantList();
+  });
+  
+  function addPlant() {
+    const nameInput = document.getElementById("plant-name") as HTMLInputElement;
+    const intervalInput = document.getElementById("water-interval") as HTMLInputElement;
+    const locationInput = document.getElementById("plant-location") as HTMLSelectElement;
+  
+    const name = nameInput.value;
+    const interval = Number(intervalInput.value);
+    const location = locationInput.value;
+  
+    if (name.trim() === "" || isNaN(interval)) {
+      return;
     }
-}
+  
+    const plant: any = {
+      name,
+      interval,
+      location,
+      lastWatered: new Date(),
+    };
+  
+    const plantList = getPlantList();
+    plantList.push(plant);
+    setPlantList(plantList);
+  
+    nameInput.value = "";
+    intervalInput.value = "";
+    locationInput.value = "";
+  }
+  
+  function getPlantList():  any[] {
+    const plantListString = localStorage.getItem("plantList");
+    if (plantListString) {
+      return JSON.parse(plantListString);
+    } else {
+      return [];
+    }
+  }
+  
+  function setPlantList(plantList: any[] = []) {
+    localStorage.setItem("plantList", JSON.stringify(plantList));
+  
+    const plantListContainer = document.getElementById("plant-list") as HTMLUListElement;
+    plantListContainer.innerHTML = "";
+  
+    for (const plant of plantList) {
+      const plantListItem = document.createElement("li");
+      plantListItem.textContent = `${plant.name} - ${plant.location}`;
+  
+      const lastWatered = new Date(plant.lastWatered);
+      const nextWatering = new Date(lastWatered.getTime() + plant.interval * 24 * 60 * 60 * 1000);
+      const daysUntilWatering = Math.round((nextWatering.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000));
+  
+      if (daysUntilWatering <= 0) {
+        plantListItem.classList.add("water-now");
+        plantListItem.setAttribute("title", "This plant needs water now");
+      } else {
+        plantListItem.setAttribute("title", `This plant needs water in ${daysUntilWatering} days`);
+      }
+  
+      plantListContainer.appendChild(plantListItem);
+    }
+  }
+  
