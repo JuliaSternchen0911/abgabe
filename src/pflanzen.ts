2b285686e114ad81f8ba548abf1cd9c19b540b10
JuliaSternchen0911 <petersjulia0911@gmail.com>
2023-05-06 12:39:57 +0200

    Reperatur der pflanzen ts und anpassen des Html

--- src/pflanzen.ts
+++ src/pflanzen.ts
@@ -50,12 +50,13 @@ function pflanzenlisteAktualisieren(pflanzen: Pflanze[]) {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
 
+    const copySource = document.getElementById('copySource') as HTMLLIElement
 
     //Für jede Pflanze in der Liste eine Listeneintrag erstellen
     for (let i = 0; i < pflanzen.length; i++) {
         let pflanze = pflanzen[i];
         let beginnDesIntervalls = new Date(pflanze.beginnDesIntervalls);
-        beginnDesIntervalls.setHours(0,0,0,0);
+        beginnDesIntervalls.setHours(0, 0, 0, 0);
         console.dir(pflanze);
         //( SekundenSeid1.1.1970 + x ) - ( SekundenSeid1.1.1970 + y)
         let dif = (date.getTime() - beginnDesIntervalls.getTime());
@@ -63,26 +64,64 @@ function pflanzenlisteAktualisieren(pflanzen: Pflanze[]) {
         //ganzzahliger rest %
         let sollGießen = difTage % pflanze.gießintervall == 0;
         //Listeneintrag erstellen
+
         let eintrag = document.createElement("li");
-        eintrag.innerHTML =
-            "<b>" +
-            pflanze.name +
-            "</b> (" +
-            pflanze.standort +
-            ")- gießen alle " +
-            pflanze.gießintervall +
-            //tenärer Operator
-            " Tage " + ((sollGießen) ? " 🌧 " : "");
-        console.dir({
-            name: pflanze.name, dif, difTage, sollGießen,datum:beginnDesIntervalls,pflanze
+        eintrag.innerHTML = copySource.innerHTML
+
+        copySource.classList.forEach(entry => {
+            eintrag.classList.add(entry)
         })
-        //Listeneintrag zur Pflanzenliste hinzufügen
+
+        const removeButton = eintrag.querySelector("[data-entry-remove='']") as HTMLButtonElement
+        removeButton.onclick = () => {
+            const pflanzeZuLoeschen = pflanze
+
+            const positionDerPflanzeZuLoeschen = i
+
+            pflanzen.splice(positionDerPflanzeZuLoeschen, 1)
+
+            // aus dem local Storage löschen  
+
+            pflanzenlisteAktualisieren(pflanzen)
+        }
+
+
+
+
+        const nameEntry = eintrag.querySelector("[data-entry-name='']") as HTMLParagraphElement
+        nameEntry.innerText = pflanze.name
+
+        const intervallEntry = eintrag.querySelector("[data-entry-intervall='']") as HTMLParagraphElement
+        intervallEntry.innerText = pflanze.gießintervall + ""
+
+        const standortEntry = eintrag.querySelector("[data-entry-standort='']") as HTMLParagraphElement
+        standortEntry.innerText = pflanze.standort
+
+        const erstellungsDatumEntry = eintrag.querySelector("[data-entry-datum='']") as HTMLTimeElement
+        erstellungsDatumEntry.innerText = beginnDesIntervalls.toDateString()
+
+        eintrag.classList.remove('invisible')
         pflanzenliste.appendChild(eintrag);
+        /*
+                let eintrag = document.createElement("li");
+                eintrag.innerHTML =
+                    "<b>" +
+                    pflanze.name +
+                    "</b> (" +
+                    pflanze.standort +
+                    ")- gießen alle " +
+                    pflanze.gießintervall +
+                    //tenärer Operator
+                    " Tage " + ((sollGießen) ? " 🌧 " : "");
+                console.dir({
+                    name: pflanze.name, dif, difTage, sollGießen,datum:beginnDesIntervalls,pflanze
+                })
+                //Listeneintrag zur Pflanzenliste hinzufügen
+                pflanzenliste.appendChild(eintrag);
+        */
     }
 }
 
 
 
 export { pflanzenHinzufuegen, pflanzenlisteAktualisieren };
-
-
